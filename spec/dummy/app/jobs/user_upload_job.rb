95250c1a18dab2a22d2a166dd6bd0ecf980687a0
Leandro Segovia <ldlsegovia@gmail.com>
2016-06-25 20:01:55 -0300

    feat(): improve error view

--- spec/dummy/app/jobs/user_upload_job.rb
+++ spec/dummy/app/jobs/user_upload_job.rb
@@ -2,6 +2,13 @@ class UserUploadJob < ActiveJob::Base
   def perform_with_feedback(result)
     sleep([*5..20].sample) # faking processing time
     return "Users loaded successfully" if result
-    raise JobNotifier::Error::Validation.new("ERROR!!!")
+
+    errors = [
+      { row: 4, errors: ["Invalid First Name", "Invalid Category"] },
+      { row: 6, errors: ["Invalid Last Name"] },
+      { row: 84, errors: ["Invalid ID"] }
+    ]
+
+    raise JobNotifier::Error::Validation.new(errors)
   end
 end
